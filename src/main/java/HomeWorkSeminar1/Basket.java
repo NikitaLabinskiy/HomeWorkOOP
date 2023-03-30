fa4731dcb6587b3ade1d8d37fc8d4d072d0da7bc
Nikita Labinskiy <NLabinskiy@gmail.com>
2023-03-30 17:12:05 +0300

    Add class Basket

---
+++ src/main/java/HomeWorkSeminar1/Basket.java
@@ -0,0 +1,24 @@
+package HomeWorkSeminar1;
+
+import java.util.Arrays;
+
+public class Basket {
+    Product[] basketWithProducts;
+
+    public Basket(Product[] basketWithProducts) {
+        this.basketWithProducts = basketWithProducts;
+    }
+
+    public Product[] getBasketWithProducts() {
+        return basketWithProducts;
+    }
+
+    public void setBasketWithProducts(Product[] basketWithProducts) {
+        this.basketWithProducts = basketWithProducts;
+    }
+
+    @Override
+    public String toString() {
+        return "Basket with products: " + Arrays.toString(basketWithProducts);
+    }
+}
